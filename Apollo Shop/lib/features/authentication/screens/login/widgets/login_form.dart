069e944ee1b8e2b101b532a3573f580314fd5adf
Nguyễn Trung Quân <116139754+Quan2808@users.noreply.github.com>
2024-07-20 03:01:36 +0700

    Add Signup Screen

--- Apollo Shop/lib/features/authentication/screens/login/widgets/login_form.dart
+++ Apollo Shop/lib/features/authentication/screens/login/widgets/login_form.dart
@@ -1,6 +1,7 @@
-
+import 'package:apolloshop/features/authentication/screens/signup/signup.dart';
 import 'package:apolloshop/utils/constants/text_strings.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 
 import '../../../../../utils/constants/sizes.dart';
@@ -14,55 +15,53 @@ class LoginForm extends StatelessWidget {
   Widget build(BuildContext context) {
     return Form(
         child: Padding(
-          padding: const EdgeInsets.symmetric(
-              vertical: TSizes.spaceBtwSections),
-          child: Column(
+      padding: const EdgeInsets.symmetric(vertical: TSizes.spaceBtwSections),
+      child: Column(
+        children: [
+          TextFormField(
+            decoration: const InputDecoration(
+              prefixIcon: Icon(Iconsax.direct_right),
+              labelText: TTexts.email,
+            ),
+          ),
+          const SizedBox(
+            height: TSizes.spaceBtwInputFields,
+          ),
+          TextFormField(
+            decoration: const InputDecoration(
+                prefixIcon: Icon(Iconsax.password_check),
+                labelText: TTexts.password,
+                suffixIcon: Icon(Iconsax.eye_slash)),
+          ),
+          const SizedBox(
+            height: TSizes.spaceBtwInputFields / 2,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              TextFormField(
-                decoration: const InputDecoration(
-                  prefixIcon: Icon(Iconsax.direct_right),
-                  labelText: TTexts.email,
-                ),
-              ),
-              const SizedBox(
-                height: TSizes.spaceBtwInputFields,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                    prefixIcon: Icon(Iconsax.password_check),
-                    labelText: TTexts.password,
-                    suffixIcon: Icon(Iconsax.eye_slash)),
-              ),
-              const SizedBox(
-                height: TSizes.spaceBtwInputFields / 2,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextButton(
-                      onPressed: () {},
-                      child: const Text(TTexts.forgetPassword))
-                ],
-              ),
-              const SizedBox(
-                height: TSizes.spaceBtwSections,
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                    onPressed: () {}, child: const Text(TTexts.signIn)),
-              ),
-              const SizedBox(
-                height: TSizes.spaceBtwItems,
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: OutlinedButton(
-                    onPressed: () {},
-                    child: const Text(TTexts.createAccount)),
-              ),
+              TextButton(
+                  onPressed: () {}, child: const Text(TTexts.forgetPassword))
             ],
           ),
-        ));
+          const SizedBox(
+            height: TSizes.spaceBtwSections,
+          ),
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+                onPressed: () {}, child: const Text(TTexts.signIn)),
+          ),
+          const SizedBox(
+            height: TSizes.spaceBtwItems,
+          ),
+          SizedBox(
+            width: double.infinity,
+            child: OutlinedButton(
+                onPressed: () => Get.to(() => const SignupScreen()),
+                child: const Text(TTexts.createAccount)),
+          ),
+        ],
+      ),
+    ));
   }
 }
--- Apollo Shop/lib/features/authentication/screens/signup/signup.dart
+++ Apollo Shop/lib/features/authentication/screens/signup/signup.dart
@@ -1,4 +1,10 @@
+import 'package:apolloshop/features/authentication/screens/signup/signup_form.dart';
+import 'package:apolloshop/utils/constants/colors.dart';
+import 'package:apolloshop/utils/constants/sizes.dart';
+import 'package:apolloshop/utils/constants/text_strings.dart';
+import 'package:apolloshop/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 class SignupScreen extends StatelessWidget {
   const SignupScreen({super.key});
@@ -6,7 +12,26 @@ class SignupScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar,
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(TSizes.defaultSpace),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              //Title
+              Text(
+                TTexts.signupTitle,
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              const SizedBox(
+                height: TSizes.spaceBtwSections,
+              ),
+              const SignupForm()
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
