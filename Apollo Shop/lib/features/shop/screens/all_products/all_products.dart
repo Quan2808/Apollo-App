58a00b786f4c76a556d75e9182b9eef31a2ef1d5
Nguyễn Trung Quân <116139754+Quan2808@users.noreply.github.com>
2024-07-30 01:23:00 +0700

    Add All Brands screen

--- Apollo Shop/lib/features/shop/screens/all_products/all_products.dart
+++ Apollo Shop/lib/features/shop/screens/all_products/all_products.dart
@@ -1,18 +1,16 @@
 import 'package:apolloshop/common/widgets/appbar/appbar.dart';
-import 'package:apolloshop/common/widgets/layouts/grid_layout.dart';
-import 'package:apolloshop/common/widgets/products/product_cards/product_card_vertical.dart';
+import 'package:apolloshop/common/widgets/products/sortable/sortable_products.dart';
 import 'package:apolloshop/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
-import 'package:iconsax/iconsax.dart';
 
 class AllProductsScreen extends StatelessWidget {
   const AllProductsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
         // App bar
-        appBar: const ApolloAppBar(
+        appBar: ApolloAppBar(
           title: Text('Popular Products'),
           showBackArrow: true,
         ),
@@ -20,27 +18,8 @@ class AllProductsScreen extends StatelessWidget {
         // Body
         body: SingleChildScrollView(
           child: Padding(
-            padding: const EdgeInsets.all(TSizes.defaultSpace),
-            child: Column(
-              children: [
-                // Drop down
-                DropdownButtonFormField(
-                  decoration:
-                      const InputDecoration(prefixIcon: Icon(Iconsax.sort)),
-                  onChanged: (value) {},
-                  items: ['Name', 'Higher Price', 'Lower Price']
-                      .map((option) =>
-                          DropdownMenuItem(value: option, child: Text(option)))
-                      .toList(),
-                ),
-                const SizedBox(height: TSizes.spaceBtwSections),
-
-                // Products
-                GridLayout(
-                    itemCount: 4,
-                    itemBuilder: (_, index) => const ProductCardVertical()),
-              ],
-            ),
+            padding: EdgeInsets.all(TSizes.defaultSpace),
+            child: SortableProducts(),
           ),
         ));
   }
--- Apollo Shop/lib/features/shop/screens/store/store.dart
+++ Apollo Shop/lib/features/shop/screens/store/store.dart
@@ -5,11 +5,13 @@ import 'package:apolloshop/common/widgets/custom_shapes/containers/search_contai
 import 'package:apolloshop/common/widgets/layouts/grid_layout.dart';
 import 'package:apolloshop/common/widgets/products/cart/cart_menu_icon.dart';
 import 'package:apolloshop/common/widgets/texts/section_heading.dart';
+import 'package:apolloshop/features/shop/screens/brands/all_brands.dart';
 import 'package:apolloshop/features/shop/screens/store/widgets/category_tab.dart';
 import 'package:apolloshop/utils/constants/colors.dart';
 import 'package:apolloshop/utils/constants/sizes.dart';
 import 'package:apolloshop/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class StoreScreen extends StatelessWidget {
   const StoreScreen({super.key});
@@ -53,7 +55,9 @@ class StoreScreen extends StatelessWidget {
                     const SizedBox(height: TSizes.spaceBtwSections),
 
                     /// Featured Brands
-                    SectionHeading(title: 'Featured Brands', onPressed: () {}),
+                    SectionHeading(
+                        title: 'Featured Brands',
+                        onPressed: () => Get.to(() => const AllBrandsScreen())),
                     const SizedBox(height: TSizes.spaceBtwItems / 1.5),
 
                     /// Brand GRID
