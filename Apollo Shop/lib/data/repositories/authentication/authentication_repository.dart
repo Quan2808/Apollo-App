3a86e747f3a90bb6867d3a51a31d7fc5ace02017
Nguyễn Trung Quân <116139754+Quan2808@users.noreply.github.com>
2024-08-02 16:41:48 +0700

    Refactor AuthenticationRepository and UserRepository
    - Moved user-related functionality from AuthenticationRepository to UserRepository.
    - Updated AuthenticationRepository to use UserRepository for user management.
    - Added UserRepository initialization in AuthenticationRepository's onInit method.
    - Refactored signOut method in AuthenticationRepository to use UserRepository.
    - Updated signIn method to handle user info fetching through UserRepository.
    - Removed redundant user management code from AuthenticationRepository.

--- Apollo Shop/lib/data/repositories/authentication/authentication_repository.dart
+++ Apollo Shop/lib/data/repositories/authentication/authentication_repository.dart
@@ -1,13 +1,10 @@
-import 'dart:convert';
-
-import 'package:apolloshop/data/models/user_model.dart';
+import 'package:apolloshop/data/repositories/user/user_repository.dart';
 import 'package:apolloshop/data/services/authentication/authentication_service.dart';
 import 'package:apolloshop/features/authentication/screens/login/login.dart';
 import 'package:apolloshop/features/authentication/screens/onboarding/onboarding.dart';
 import 'package:apolloshop/navigation_menu.dart';
 import 'package:apolloshop/utils/constants/api_constants.dart';
 import 'package:apolloshop/utils/helpers/helper_exception.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get/get.dart';
@@ -16,14 +13,19 @@ import 'package:get_storage/get_storage.dart';
 class AuthenticationRepository extends GetxController {
   static AuthenticationRepository get instance => Get.find();
 
-  // Variables
   final AuthenticationService _authService =
-      AuthenticationService(ApiConstants.baseApiUrl);
+  AuthenticationService(ApiConstants.baseApiUrl);
   final deviceStorage = GetStorage();
-
   final _secureStorage = const FlutterSecureStorage();
-  final Rx<UserModel?> _currentUser = Rx<UserModel?>(null);
-  final RxString accessToken = ''.obs;
+
+  // Create an instance of UserRepository
+  late final UserRepository _userRepository;
+
+  @override
+  void onInit() {
+    super.onInit();
+    _userRepository = UserRepository(_authService, _secureStorage);
+  }
 
   @override
   void onReady() {
@@ -36,8 +38,8 @@ class AuthenticationRepository extends GetxController {
       final storedUser = await _secureStorage.read(key: 'user');
       final storedToken = await _secureStorage.read(key: 'accessToken');
       if (storedUser != null && storedToken != null) {
-        _currentUser.value = UserModel.fromJson(jsonDecode(storedUser));
-        accessToken.value = storedToken;
+        _userRepository.accessToken.value = storedToken;
+        await _userRepository.fetchUserInfo();
         Get.offAll(() => const NavigationMenu());
       } else {
         bool isFirstTime = deviceStorage.read('firstStartUp') ?? true;
@@ -52,7 +54,6 @@ class AuthenticationRepository extends GetxController {
     }
   }
 
-
   /* ----------------------- AUTHENTICATION ----------------------- */
 
   // Sign In
@@ -66,12 +67,10 @@ class AuthenticationRepository extends GetxController {
         password: password,
       );
 
-      // Save accessToken
-      accessToken.value = response['accessToken'];
-      await _secureStorage.write(key: 'accessToken', value: accessToken.value);
+      _userRepository.accessToken.value = response['accessToken'];
+      await _secureStorage.write(key: 'accessToken', value: _userRepository.accessToken.value);
 
-      // Fetch user info using the token
-      await _fetchUserInfo();
+      await _userRepository.fetchUserInfo();
     } catch (e) {
       throw HelperException.getLoginException(e.toString());
     }
@@ -98,34 +97,8 @@ class AuthenticationRepository extends GetxController {
 
   // Sign Out
   Future<void> signOut() async {
-    _currentUser.value = null;
-    accessToken.value = '';
-
-    // Clear stored user and token
-    await _secureStorage.delete(key: 'user');
-    await _secureStorage.delete(key: 'accessToken');
-
+    await _userRepository.signOut();
     deviceStorage.write('firstStartUp', false);
     Get.offAll(() => const LoginScreen());
   }
-
-  Future<void> _fetchUserInfo() async {
-    try {
-      final userInfo = await _authService.fetchUserInfo(accessToken.value);
-      _currentUser.value = userInfo;
-      await _secureStorage.write(key: 'user', value: jsonEncode(_currentUser.value!.toJson()));
-    } catch (e) {
-      if (kDebugMode) {
-        print('====================== BEGIN PROBLEMS ======================');
-        print(e.toString());
-        print('====================== END PROBLEMS ======================');
-      }
-      rethrow;
-    }
-  }
-
-  UserModel? get currentUser => _currentUser.value;
-
-  Future<bool> get isSignedIn async =>
-      await _secureStorage.read(key: 'accessToken') != null;
 }
--- Apollo Shop/lib/data/repositories/user/user_repository.dart
+++ Apollo Shop/lib/data/repositories/user/user_repository.dart
@@ -1,7 +1,36 @@
+import 'dart:convert';
+
+import 'package:apolloshop/data/models/user_model.dart';
 import 'package:apolloshop/data/services/authentication/authentication_service.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get/get.dart';
 
 class UserRepository extends GetxController {
-  static UserRepository get instance => Get.find();
-  final authService = Get.find<AuthenticationService>();
+  final AuthenticationService _authService;
+  final FlutterSecureStorage _secureStorage;
+
+  UserRepository(this._authService, this._secureStorage);
+
+  final Rx<UserModel?> _currentUser = Rx<UserModel?>(null);
+  final RxString accessToken = ''.obs;
+
+  UserModel? get currentUser => _currentUser.value;
+
+  Future<void> fetchUserInfo() async {
+    try {
+      final userInfo = await _authService.fetchUserInfo(accessToken.value);
+      _currentUser.value = userInfo;
+      await _secureStorage.write(key: 'user', value: jsonEncode(_currentUser.value!.toJson()));
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<void> signOut() async {
+    _currentUser.value = null;
+    accessToken.value = '';
+
+    await _secureStorage.delete(key: 'user');
+    await _secureStorage.delete(key: 'accessToken');
+  }
 }
