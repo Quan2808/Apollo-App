9490f83d4fbe6af19216953b7d11508e75f60a8d
Nguyễn Trung Quân <116139754+Quan2808@users.noreply.github.com>
2024-08-09 04:09:34 +0700

    Handle null assignment in VariantController
    - Updated `selectedVariant` to allow `null` values by initializing it with `Rx<VariantModel?>(null)` instead of `VariantModel.empty().obs`.
    - This change prevents the `Null` is not a subtype of type `VariantModel` error when resetting the selected variant.

--- Apollo Shop/lib/features/shop/controllers/product/variant_controller.dart
+++ Apollo Shop/lib/features/shop/controllers/product/variant_controller.dart
@@ -10,7 +10,8 @@ class VariantController extends GetxController {
   final isLoading = false.obs;
   final VariantRepository _variantRepository = Get.find();
   RxList<VariantModel> variants = <VariantModel>[].obs;
-  Rx<VariantModel?> selectedVariant = VariantModel.empty().obs;
+  Rx<VariantModel?> selectedVariant = Rx<VariantModel?>(null);
+
   Future<void> fetchVariantsByProduct(int productId) async {
     try {
       isLoading.value = true;
